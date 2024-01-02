61ea4e2eca465e38cf891be6a39b1dbf57686e12
zz18000156218 <99482971+zz18000156218@users.noreply.github.com>
2024-01-02 19:50:56 +0800

    Create 朴素贝叶斯评估.R

---
+++ 朴素贝叶斯评估.R
@@ -0,0 +1,52 @@
+setwd("C:/周州的数据/PFS预测模型")
+library(tidymodels)
+library('caret')
+library(pROC)
+library(ROCR)
+library(Hmisc)
+library(dplyr)
+library(mclust)
+library(bcrm)
+data2<-read.csv("lasso.csv",header = TRUE)
+data2$class<-factor(data2$class,levels=c(0,1),labels=c("NO",'YES'))
+data2$classification<-factor(data2$classification,levels=c(1,2,3,4),labels=c('SQCC','ADC','LCC','SCLC'))
+data2$gender<-factor(data2$gender,levels=c(0,1),labels=c('male','female'))
+x_train<-data2[,1:10]
+y_train<-data2[,11]
+set.seed(825)
+fitControl=trainControl(method='cv',
+                        number=10,
+                        classProbs = TRUE,
+                        summaryFunction=twoClassSummary,
+                        search='random'
+)
+rf1=train(x=x_train,y=y_train,
+          method='naive_bayes',
+          trControl=fitControl,
+          verbose=FALSE,
+          metric='ROC',
+          tuneLength = 30)
+names(getModelInfo())
+data1<-read.csv("Elasso.csv",header = TRUE)
+data1$class<-factor(data1$class,levels=c(0,1),labels=c("NO",'YES'))
+data1$classification<-factor(data1$classification,levels=c(1,2,3,4),labels=c('SQCC','ADC','LCC','SCLC'))
+data1$gender<-factor(data1$gender,levels=c(0,1),labels=c('male','female'))
+x_test<-data1[,1:10]
+y_test<-data1[,11]
+data.predict1=predict(rf1,newdata=x_test,type='prob')
+data.predict<-ifelse(data.predict1[,2]>0.625,1,0)
+confusionMatrix(factor(data.predict,levels=c(0,1),labels=c("NO",'YES')),y_test,positive = 'YES')
+ci_sen<-binconf(24,26)
+ci_sen
+ci_spe<-binconf(64,74)
+ci_spe
+pre3<-predict(rf1,newdata=x_test,type='prob')
+pred3<-prediction(pre3[,2],y_test)
+performance(pred3,'auc')@y.values
+auc1<-roc(y_test,pre3[,2],levels=c('NO','YES'),direction='<')
+auc1
+auc2<-ci.auc(auc1)
+auc2
+cutoff=auc1$thresholds[which.max(auc1$sensitivities+auc1$specificities)]
+brier<-BrierScore(pre3,y_test)
+brier
